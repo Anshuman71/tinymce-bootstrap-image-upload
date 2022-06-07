4cb5e411a5531d936e0b89e28fb68869903e5f9b
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-06-07 07:48:39 +0530

    Create image upload with TinyMCE using Bootstrap

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const server = express();
+const PORT = 8080;
+const multer = require("multer");
+const { nanoid } = require("nanoid");
+
+server.use(express.static("public"));
+
+server.use("/images", express.static("images"));
+
+// configure storage options for multer
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images"); //important this is a direct path from our current file to storage location
+  },
+  filename: (req, file, cb) => {
+    // get the file extension from mimetype
+    const fileExtension = file.mimetype.split("/")[1];
+    // create a unique file name using 10 random characters joined with time in milliseconds
+    const uniqueFileName = `${nanoid(10)}-${Date.now()}.${fileExtension}`;
+    cb(null, uniqueFileName);
+  },
+});
+
+// initialize multer with the storage engine configuration
+const upload = multer({ storage: fileStorageEngine });
+
+// handle image uploads using multer and return the `imageUrl` as response
+server.post("/upload-image", upload.single("image"), (request, response) => {
+  response.send({
+    imageUrl: `${request.headers.origin}/${request.file.path}`,
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running at port: ${PORT}`);
+});
