374a9e9401a9a7c37497170b87a3607f8acc244c
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-07-22 07:09:17 +0530

    Update to match with code example

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const PORT = 8080;
 const multer = require("multer");
 const { nanoid } = require("nanoid");
 
+// serve HTML file from /public folder at "/" path
 server.use(express.static("public"));
 
+// serve the uploaded images from ./images folder at "./images" path
 server.use("/images", express.static("images"));
 
 // configure storage options for multer
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./images"); //important this is a direct path from our current file to storage location
+    cb(null, "./images"); // relative path to storage location
   },
   filename: (req, file, cb) => {
     // get the file extension from mimetype
@@ -25,13 +27,14 @@ const fileStorageEngine = multer.diskStorage({
 // initialize multer with the storage engine configuration
 const upload = multer({ storage: fileStorageEngine });
 
-// handle image uploads using multer and return the `imageUrl` as response
+// handle image upload using multer and return the `imageUrl` as response
 server.post("/upload-image", upload.single("image"), (request, response) => {
   response.send({
     imageUrl: `${request.headers.origin}/${request.file.path}`,
   });
 });
 
+// start the server at http://localhost:{PORT}
 server.listen(PORT, () => {
   console.log(`server is running at port: ${PORT}`);
 });
